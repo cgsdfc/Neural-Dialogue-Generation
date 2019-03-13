52fa389b69c27c9842be5ddf714c6ed050825a04
cgsdfc <cgsdfc@126.com>
2019-03-13 10:44:49 +0800

    Synchronize flags in README and in parse*.lua
    add missing help of flags in code from README.
    add missing flags description in README from code.

--- Atten/parse.lua
+++ Atten/parse.lua
@@ -1,5 +1,6 @@
 local stringx = require('pl.stringx')
 local cmd = torch.CmdLine()
+
 cmd:option("-batch_size", 128, "batch size")
 cmd:option("-dimension", 512, "vector dimensionality")
 cmd:option("-dropout", 0.2, "dropout rate")
@@ -14,18 +15,20 @@ cmd:option("-vocab_source", 25010, "source vocabulary size")
 cmd:option("-vocab_target", 25010, "target vocabulary size")
 cmd:option("-thres", 5, "gradient clipping thres")
 cmd:option("-max_iter", 8, "max number of iteration")
-cmd:option("-source_max_length", 50, "")
-cmd:option("-target_max_length", 50, "")
-cmd:option("-layers", 2, "")
-cmd:option("-saveFolder", "save", "")
+cmd:option("-source_max_length", 50, "max length of source sentences")
+cmd:option("-target_max_length", 50, "max length of target sentences")
+cmd:option("-layers", 2, "number of lstm layers")
+cmd:option("-saveFolder", "save", "the folder to save models and parameters")
 cmd:option("-reverse", false, "whether to reverse the sources")
 cmd:option("-gpu_index", 1, "the index of GPU to use")
-cmd:option("-saveModel", true, "")
-cmd:option("-dictPath", "../data/movie_25000", "")
+cmd:option("-saveModel", true, "whether to save the trained model")
+cmd:option("-dictPath", "../data/movie_25000", "dictionary file")
+
 local params = cmd:parse(arg)
 params.save_prefix = params.saveFolder .. "/model"
 params.save_params_file = params.saveFolder .. "/params"
 params.output_file = params.saveFolder .. "/log"
 paths.mkdir(params.saveFolder)
 print(params)
+
 return params;
--- Decode/decode_parse.lua
+++ Decode/decode_parse.lua
@@ -1,29 +1,32 @@
+-- Define command line options and parse them.
 local stringx = require('pl.stringx')
 local cmd = torch.CmdLine()
+
 cmd:option("-beam_size", 7, "beam_size")
 cmd:option("-batch_size", 128, "decoding batch_size")
-cmd:option("-params_file", "", "")
-cmd:option("-model_file", "", "")
+cmd:option("-params_file", "", "input parameter files for a pre-trained Seq2Seq model")
+cmd:option("-model_file", "", "path for loading a pre-trained Seq2Seq model")
 cmd:option("-setting", "BS", "setting for decoding, sampling, BS, DiverseBS,StochasticGreedy")
-cmd:option("-DiverseRate", 0, "")
-cmd:option("-InputFile", "../data/t_given_s_test.txt", "")
-cmd:option("-OutputFile", "output.txt", "")
-cmd:option("-max_length", 20, "")
-cmd:option("-min_length", 0, "")
+cmd:option("-DiverseRate", 0, "The diverse-decoding rate for penalizing intra-sibling hypotheses in the diverse decoding model")
+cmd:option("-InputFile", "../data/t_given_s_test.txt", "the input file for decoding")
+cmd:option("-OutputFile", "output.txt", "the output file to store the generated responses")
+cmd:option("-max_length", 20, "the maximum length of a decoded response")
+cmd:option("-min_length", 0, "the minimum length of a decoded response")
 cmd:option("-NBest", false, "output N-best list or just a simple output")
 cmd:option("-gpu_index", 1, "the index of GPU to use")
 cmd:option("-allowUNK", false, "whether allowing to generate UNK")
-cmd:option("-MMI", false, "")
-cmd:option("-onlyPred", true, "")
-cmd:option("-MMI_params_file", "../atten/save_s_given_t/params", "")
-cmd:option("-MMI_model_file", "", "")
-cmd:option("-max_decoded_num", 0, "")
-cmd:option("-output_source_target_side_by_side", true, "")
-cmd:option("-StochasticGreedyNum", 1, "")
+cmd:option("-MMI", false, "whether to perform the mutual information reranking after decoding")
+cmd:option("-onlyPred", true, "") -- Not found in readme.
+cmd:option("-MMI_params_file", "../atten/save_s_given_t/params", "the input parameter file for training the backward model p(s|t)")
+cmd:option("-MMI_model_file", "", "path for loading the backward model p(s|t)")
+cmd:option("-max_decoded_num", 0, "the maximum number of instances to decode. decode the entire input set if the value is set to 0")
+cmd:option("-output_source_target_side_by_side", true, "output input sources and decoded targets side by side")
+cmd:option("-StochasticGreedyNum", 1, "") -- Not found in readme.
 cmd:option("-target_length", 0, "force the length of the generated target, 0 means there is no such constraints")
-cmd:option("-dictPath", "../data/movie_25000", "")
-cmd:option("-PrintOutIllustrationSample", false, "")
+cmd:option("-dictPath", "../data/movie_25000", "dictionary file")
+cmd:option("-PrintOutIllustrationSample", false, "") -- Not found in readme.
 
 local params = cmd:parse(arg)
 print(params)
+
 return params;
