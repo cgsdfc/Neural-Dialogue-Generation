395caeb4523f354b1b131f6674daea16a99446c3
cgsdfc <cgsdfc@126.com>
2019-04-14 21:07:00 +0800

    Fix wrong saveFolder of train_forward.sh

--- exp/MMI/train_forward.sh
+++ exp/MMI/train_forward.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-DATA_ROOT=/home/cgsdfc/OpenSubData/dialogue_length2_3
+DATA_ROOT=/home/cgsdfc/OpenSubData/
 SAVE_ROOT=/home/cgsdfc/SavedModels/Neural-Dialogue-Generation
 
-BATCH_SIZE=20
+BATCH_SIZE=60 # This is the golden number
+DATASET=dialogue_length2_6
+DATA_ROOT=$DATA_ROOT/$DATASET
+
 DIMENSION=1000
 DROP_OUT=0.2
 INIT_WEIGHT=0.08
@@ -14,11 +17,11 @@ GPU_INDEX=1
 THRES=1
 MAX_ITER=8
 
-SAVE_FOLDER=$SAVE_ROOT/MMI-forward-dialen_2_3
+SAVE_FOLDER=$SAVE_ROOT/MMI-forward-$DATASET
 
-TRAIN_FILE=$DATA_ROOT/s_given_t_dialogue_length2_3_train.txt
-DEV_FILE=$DATA_ROOT/s_given_t_dialogue_length2_3_dev.txt
-TEST_FILE=$DATA_ROOT/s_given_t_dialogue_length2_3_test.txt
+TRAIN_FILE=$DATA_ROOT/train.txt
+DEV_FILE=$DATA_ROOT/dev.txt
+TEST_FILE=$DATA_ROOT/test.txt
 
 th Atten/train.lua \
     -batch_size $BATCH_SIZE \
@@ -34,4 +37,4 @@ th Atten/train.lua \
     -dev_file $DEV_FILE \
     -test_file $TEST_FILE \
     -max_iter $MAX_ITER \
-    -start_halve $START_HALVE
+    -start_halve $START_HALVE \
